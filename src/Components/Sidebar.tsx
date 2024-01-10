e7a47c93bf0d434bb2961aec590505c75c382754
Thiago Leite <thiago.0686@gmail.com>
2024-01-09 23:30:33 -0300

    feat: create profile avatar

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -4,9 +4,14 @@ export function Sidebar() {
     return (
         <>
             <aside className={styles.sidebar}>
-                <img src="" alt="" />
+                <img
+                    className={styles.cover}
+                    src="https://images.unsplash.com/photo-1682686580452-37f1892ee5e8" />
 
                 <div className={styles.profile}>
+                    <img
+                        className={styles.avatar}
+                        src="https://github.com/thiagoleite06.png" />
                     <strong>Thiago Leite</strong>
                     <span>Web/Mobile Developer</span>
                 </div>
